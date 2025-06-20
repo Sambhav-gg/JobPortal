5e13d2164cb0ac8e85c120747f0b22be05baa95a
Sambhav-gg <156508582+Sambhav-gg@users.noreply.github.com>
2025-06-20 22:20:25 +0530

    v1

---
+++ backend/index.js
@@ -0,0 +1,49 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.routes.js";
+import companyRoute from "./routes/company.routes.js";
+import jobRoute from "./routes/job.routes.js";
+import applicationRoute from "./routes/application.route.js";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+const _dirname = path.resolve();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// CORS Setup
+const corsOptions = {
+    origin: "http://localhost:5173",
+    credentials: true
+};
+app.use(cors(corsOptions));
+
+// API Routes
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
+// Serve Frontend Build Files
+app.use(express.static(path.join(_dirname, "frontend", "dist")));
+
+// Wildcard Route (keep this LAST)
+// app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+// });
+
+// Start Server
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
+});
---
+++ backend/middlewares/isAuthenticated.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+
+ const isAuthenticated =async(req,res,next)=>{
+    try{
+        const token =req.cookies.token;
+        if(!token){
+            return res.status(400).json({
+                message:"User is not authenticated ",
+                success:false
+            })
+        }
+        //ab hum token ko decode krenge 
+        const decode =jwt.verify(token,process.env.SECRET_KEY);
+        if(!decode){
+            return res.status(400).json({
+                message:"Invalid token data",
+                status:false
+            })
+        };
+
+        
+        req.user = { _id: decode.userId };
+        next();
+         
+    }
+    catch(error){
+        console.log(error);
+
+    }
+ }
+ export default isAuthenticated;
