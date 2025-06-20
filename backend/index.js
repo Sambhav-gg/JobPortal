1aae12d8b776332b90188d576200141ad97b8c9f
Sambhav Garg <156508582+Sambhav-gg@users.noreply.github.com>
2025-06-21 00:43:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,11 +21,7 @@ app.use(cookieParser());
 
 // ✅ CORS Setup
 const corsOptions = {
-    origin: [
-        "http://localhost:5173",
-        "https://jobportal-gg.onrender.com", // Your frontend on Render
-        "https://job-portal-frontend-six-opal.vercel.app" // If you still deploy on Vercel later
-    ],
+    origin: ["https://jobportal-gg.onrender.com"],
     credentials: true, // Allow cookies to be sent
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
