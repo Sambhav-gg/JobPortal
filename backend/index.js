fe6466c8db63352dd01ae6d04c9e7faaea542640
Sambhav Garg <156508582+Sambhav-gg@users.noreply.github.com>
2025-06-20 22:57:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 // CORS Setup
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://jobportal-gg.onrender.com/",
     credentials: true
 };
 app.use(cors(corsOptions));
